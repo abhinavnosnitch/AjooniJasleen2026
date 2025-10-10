2e2d2588d85674ebcc519ffa876ac32a42fad605
musicabnv <musicabnv@gmail.com>
2025-10-10 15:49:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'playfair': ['Playfair Display', 'serif'],
+        'cormorant': ['Cormorant Garamond', 'serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'luxury': {
+          'gold': '#C0A062',
+          'charcoal': '#2E2E2E',
+          'ivory': '#F9F7F3',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
